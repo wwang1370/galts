ac0e033c1c11d79af7ca02fffffa43eccba35d0f
Mehmet Hakan Satman <mhsatman@istanbul.edu.tr>
2011-02-04 00:00:00 +0000

    version 1.1

--- R/galts.r
+++ R/galts.r
@@ -1,50 +1,58 @@
-require("genalg");
-
-ga.lts<-function(formula, h=NULL, iters=2, popsize=50, lower , upper , csteps=2){
-
-	ols<-lm(formula,x=TRUE,y=TRUE);
-	x<-ols$x;
-	y<-ols$y;
-	n<-length(y);
-	p<-dim(x)[2];
-	ind<-rep(0,n);
-	if(is.null(h)){
-		h<-floor(n/2)+floor((p+1)/2);
-	}
-
-	cstep<-function(candidates){
-		cmybetas<-candidates;
-		res<-y-x%*%cmybetas;
-		indices<-order(abs(res))[1:p];
-		for (i in 1:csteps){
-			ind<<-rep(0,n);
-			ind[indices]<<-1;
-			ols<-lm(formula=y~x-1, weights=ind);
-			mybetas<-ols$coefficients;
-			res<-y-x%*%mybetas;
-			res2<-abs(res);
-			o<-order(res2);
-			indices<-sort(o[1:h]);
-		}
-		return(mybetas);
-	}
+require("genalg")
+require("DEoptim")
 
 
-	cost<-function(candidates){
-		newbetas<-cstep(candidates);
-		res<-y-x%*%newbetas;
-		fitn<-sum(sort(res^2)[1:h]);
-		return(fitn);	
-	}
+ga.lts<-function(formula, h=NULL, iters=2, popsize=50, lower , upper , csteps=2, method="ga", verbose=FALSE){
+    x<-model.matrix(formula)
+    y<-model.frame(formula)[,1]
+    n<-length(y)
+    p<-dim(x)[2]
+    ind<-rep(0,n)
+    if(is.null(h)){
+        h<-floor(n/2)+floor((p+1)/2)
+    }
+    if(is.null(method) ||  (method!="de" && method!="ga")){
+        cat("Please select a method: 'de' for differential evolution or 'ga' for genetic algorithm\n")
+        return(NULL)
+    }
 
-	
-	ga<-rbga(stringMin=rep(lower,p), stringMax=rep(upper,p), evalFunc=cost, iters=iters, popSize=popsize);
-	best<-ga$population[1,];
-	csteps<<-10;
-	newbetas<-cstep(best);	
-	res<-y-x%*%newbetas;
-	crit<-sum(sort(res^2)[1:h]);
-	result<-list(coefficients=newbetas, crit=crit);
-	return(result);
+   cstep<-function(candidates, csteps){
+        cmybetas<-candidates
+        indices<-order(abs(y-x%*%cmybetas))[1:p]
+        for (i in 1:csteps){
+            ols<-lm(y[indices]~x[indices,]-1)
+            mybetas<-ols$coefficients
+            res<-y-x%*%mybetas
+            res2<-abs(res)
+            o<-order(res2)
+            indices<-sort(o[1:h])
+        }
+        return(mybetas)
+    }
+
+    cost<-function(candidates){
+        newbetas<-cstep(candidates, csteps)
+        res<-y-x%*%newbetas
+        fitn<-sum(sort(res^2)[1:h])
+        return(fitn)
+    }
+
+    best<-rep(0,p)
+    if(method=="ga"){   
+        ga<-rbga(stringMin=rep(lower,p), stringMax=rep(upper,p), evalFunc=cost, iters=iters, popSize=popsize, verbose=verbose)
+        best<-ga$population[1,]
+    }else if(method=="de"){
+        de<-DEoptim(fn=cost, lower=rep(lower,p), upper=rep(upper,p), control=DEoptim.control(itermax=iters, NP=popsize, trace=verbose))
+        best<-de$optim$bestmem       
+	 }
+	 newbetas<-cstep(best,10)
+    res<-y-x%*%newbetas
+    crit<-sum(sort(res^2)[1:h])
+    result<-list(coefficients=as.double(newbetas), crit=crit, method=method)
+    return(result)
 }
+
+
+
+
 
